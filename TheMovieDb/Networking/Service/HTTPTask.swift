72efc977758b01867e10d3594d6040ec3b321f78
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-11-05 22:16:19 -0400

    Implement Network layer with only getMovies endpoint

---
+++ TheMovieDb/Networking/Service/HTTPTask.swift
@@ -0,0 +1,32 @@
+//
+//  HTTPTask.swift
+//  TheMovieDb
+//
+//  Created by Reddy Tintaya Conde on 5/11/21.
+//
+
+import Foundation
+
+
+public enum HTTPTask {
+    case request
+    case requestParameters(bodyParameters: Parameters?, urlParameters: Parameters?)
+    case requestParametersAndHeaders(bodyParamenters: Parameters?, urlParameters: Parameters?, additionalHeaders: HTTPHeaders?)
+}
+
+public typealias HTTPHeaders = [String:String]
+
+
+public typealias Parameters = [String:Any]
+
+
+public protocol ParameterEncoder {
+    static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
+}
+
+
+public enum NetworkError: String, Error {
+    case parametersNil = "Parameters were nil."
+    case encodingFailed = "Parameter encoding failed."
+    case missingURL = "URL is nil."
+}
